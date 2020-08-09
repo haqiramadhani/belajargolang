72717166d2e3cc2994f6d4e714d9ac13e433a730
Ezhause <hakiramadhani@gmail.com>
2020-08-09 21:14:47 +0700

    Initial commit

---
+++ datatype.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)
+	fmt.Println(1.1)
+
+	fmt.Println("Haqi")
+	fmt.Println("Haqi Ramadhani")
+
+	fmt.Println(true)
+	fmt.Println(false)
+}

a99269d951396109aa2f3dd04febfb58ea683ed1
Ezhause <hakiramadhani@gmail.com>
2020-08-09 21:21:32 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar Go-lang")
+	fmt.Println("Belajar" + " " + "Go-lang")
+	fmt.Println(len("Belajar"))
+	fmt.Println("Belajar"[0])
+	fmt.Println("Belajar"[0:4])
+	fmt.Println("Belajar"[4:])
+	fmt.Println("Belajar"[:4])
+}
